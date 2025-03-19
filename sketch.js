12fd61bebbfc35fc1014f2b48a58cfa1f75c6dba
ThisIsSebi <shoff439@gmail.com>
2025-03-19 11:26:46 +0100

    Ball moves from left to right and tried to make it move randomly

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+var x = 0;
+var y = 0;
+var speed = 1;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  ellipse(x, y, 100, 100);
+
+  if(x > width){
+    speed = -1;
+    x = random(0,400);
+    y = random(0,400)
+    
+  }
+  if(x <= 0){
+    speed = 1;
+    y = random(0,400)
+  }
+  if(y > 400){
+    speed = -1;
+    y = random(0, 400)
+  }
+
+  if(y <= 0){
+    speed = -1;
+  }
+  
+  x = x + speed;
+  y = y + speed;
+ 
 }
